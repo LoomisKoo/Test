a5c7131997d481777f06c0d7446c7d41de7acd9a
LoomisKoo <286305974@qq.com>
2018-11-14 09:35:05 +0800

    增加aroute

--- app/src/main/java/com/example/administrator/test/activity/SecondActivity.java
+++ app/src/main/java/com/example/administrator/test/activity/SecondActivity.java
@@ -1,25 +1,22 @@
 package com.example.administrator.test.activity;
 
 import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.alibaba.android.arouter.launcher.ARouter;
+import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.administrator.test.R;
 import com.example.administrator.test.base.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+/**
+ * @author
+ */
 
+@Route(path = "/com/Activity1")
+public class SecondActivity extends BaseActivity {
     @Override
     public void widgetClick(View v) {
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -34,7 +31,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public int bindLayout() {
-        return R.layout.activity_main;
+        return R.layout.activity_second;
     }
 
     @Override
@@ -47,7 +44,6 @@ public class MainActivity extends BaseActivity {
 
     }
 
-
     @Override
     public void setListener() {
 
@@ -55,18 +51,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void onMenuClickListener(int menuId) {
-        switch (menuId) {
-            case R.id.action_search:
-                ARouter.getInstance().build("/com/Activity1").navigation();
-//                startActivity(new Intent(MainActivity.this,SecondActivity.class));
-                break;
-            case R.id.action_notification:
-                showToast("Notification");
-                break;
 
-            default:
-                break;
-        }
     }
 
     @Override
