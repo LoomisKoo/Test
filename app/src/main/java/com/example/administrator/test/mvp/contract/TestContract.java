5cb0a2dd28ba7f334c60a645d0ac7826cb22b69b
LoomisKoo <286305974@qq.com>
2018-11-21 10:26:28 +0800

    抽取lifecycle接口作为基类

--- app/src/main/java/com/example/administrator/test/mvp/contract/TestContract.java
+++ app/src/main/java/com/example/administrator/test/mvp/contract/TestContract.java
@@ -1,12 +1,10 @@
 package com.example.administrator.test.mvp.contract;
 
-import android.arch.lifecycle.Lifecycle;
-import android.arch.lifecycle.LifecycleObserver;
-import android.arch.lifecycle.LifecycleOwner;
-import android.arch.lifecycle.OnLifecycleEvent;
+import com.example.administrator.test.mvp.base.IBaseLifecycleI;
 
-import org.jetbrains.annotations.NotNull;
-
+/**
+ * @author koo
+ */
 public interface TestContract {
     interface Model {
         double add(double a, double b);
@@ -16,53 +14,7 @@ public interface TestContract {
         void addResult(double result);
     }
 
-    interface Presenter extends LifecycleObserver {
+    interface Presenter extends IBaseLifecycleI {
         void calculate(double a, double b);
-
-        /**
-         * Activity的onCreate
-         * @param owner
-         */
-        @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-        void onCreate(@NotNull LifecycleOwner owner);
-        /**
-         * Activity的onStart
-         * @param owner
-         */
-        @OnLifecycleEvent(Lifecycle.Event.ON_START)
-        void onStart(@NotNull LifecycleOwner owner);
-        /**
-         * Activity的onResume
-         * @param owner
-         */
-        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        void onResume(@NotNull LifecycleOwner owner);
-        /**
-         * Activity的onPause
-         * @param owner
-         */
-        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-        void onPause(@NotNull LifecycleOwner owner);
-        /**
-         * Activity的onStop
-         * @param owner
-         */
-        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-        void onStop(@NotNull LifecycleOwner owner);
-        /**
-         * Activity的onStop
-         * @param owner
-         */
-        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        void onDestroy(@NotNull LifecycleOwner owner);
-
-        /**
-         * Activity的生命周期改变
-         * @param owner
-         * @param event
-         */
-        @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
-        void onLifecycleChanged(@NotNull LifecycleOwner owner,
-                                @NotNull Lifecycle.Event event);
     }
 }
